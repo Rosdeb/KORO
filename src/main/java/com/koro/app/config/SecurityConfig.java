27ed2fc264c360b3c612530dc10da9bfb407c7e9
Rosdeb koch <168054592+Rosdeb@users.noreply.github.com>
2026-08-25 12:45:05 +0600

    security cross state error fixed

--- src/main/java/com/koro/app/config/SecurityConfig.java
+++ src/main/java/com/koro/app/config/SecurityConfig.java
@@ -59,7 +59,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "http://localhost:8080",
+                "https://koro-website.rosdekoc12.workers.dev"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
